8354fc20876a706529fe595ccc54c54084efd4a9
kripesh poudel <pkripesh345@gmail.com>
2022-08-11 11:48:25 +0545

    new changes

--- newpython.py
+++ newpython.py
@@ -8,5 +8,6 @@ we can write any thing here
 a = 5 #here, a is a variable which stores/holds integer typed value i.e 5
 naam = "kripesh" #here, naam is a variable which stores/olds string type value i.e "kripesh"
 
-
+# and in python string are always inside single or double quotes"
+# for eg: "kripesh" or 'kripesh'
 
