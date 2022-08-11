b71f06d82d90cead2c212c6668676e9f46f800e1
kripesh poudel <pkripesh345@gmail.com>
2022-08-11 11:41:37 +0545

    string in pthn

---
+++ newpython.py
@@ -0,0 +1,12 @@
+# ---> it represent a comment in python
+
+"""
+it represent a multi-line comment 
+we can write any thing here 
+"""
+
+a = 5 #here, a is a variable which stores/holds integer typed value i.e 5
+naam = "kripesh" #here, naam is a variable which stores/olds string type value i.e "kripesh"
+
+
+
